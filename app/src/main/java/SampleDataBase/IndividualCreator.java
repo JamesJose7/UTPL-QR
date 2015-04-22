ecad6a9aff896cf98646e668e91d09c374de1a53
Jose <pepe.96.jose@gmail.com>
2015-04-22 18:15:55 -0500

    Test project: transit scanner

--- app/src/main/java/SampleDataBase/IndividualCreator.java
+++ app/src/main/java/SampleDataBase/IndividualCreator.java
@@ -9,14 +9,16 @@ public class IndividualCreator {
     private final String NO_ANTECEDENTES = "No antecedentes";
     private final String ANTECEDENTES_INFRACCIONES = "Infraccionado";
 
-    Individual firstIndividual = new Individual("Jaime", NO_ANTECEDENTES, 001, "ADG-584");
-    Individual secondIndividual = new Individual("Pedro", ANTECEDENTES_INFRACCIONES, 002, "LJG-885");
-    Individual thirdIndividual = new Individual("Travis", NO_ANTECEDENTES, 003, "FFK-737");
-    Individual fourthIndividual = new Individual("Javier", NO_ANTECEDENTES, 004, "GSC-853");
-    Individual fifthIndividual = new Individual("James", ANTECEDENTES_INFRACCIONES, 005, "LOJ-235");
+    Individual firstIndividual = new Individual("Jaime", NO_ANTECEDENTES, 1, "ADG-584");
+    Individual secondIndividual = new Individual("Pedro", ANTECEDENTES_INFRACCIONES, 2, "LJG-885");
+    Individual thirdIndividual = new Individual("Travis", NO_ANTECEDENTES, 3, "FFK-737");
+    Individual fourthIndividual = new Individual("Javier", NO_ANTECEDENTES, 4, "GSC-853");
+    Individual fifthIndividual = new Individual("James", ANTECEDENTES_INFRACCIONES, 5, "LOJ-235");
 
 
     public Individual[] individuals = {firstIndividual, secondIndividual, thirdIndividual, fourthIndividual, fifthIndividual};
 
-
+    public Individual[] getIndividuals() {
+        return individuals;
+    }
 }
