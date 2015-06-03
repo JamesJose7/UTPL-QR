ca903eb46efec911ec8802e33688268c1b51fcf2
Jose <pepe.96.jose@gmail.com>
2015-06-03 17:43:26 -0500

    Add new icons and exit mode in full image activity

--- app/src/main/java/com/example/jose/utplqr/DisplayFullImage.java
+++ app/src/main/java/com/example/jose/utplqr/DisplayFullImage.java
@@ -1,5 +1,6 @@
 package com.example.jose.utplqr;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
@@ -7,9 +8,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -18,6 +22,9 @@ import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListene
 
 
 public class DisplayFullImage extends ActionBarActivity {
+
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,22 @@ public class DisplayFullImage extends ActionBarActivity {
         Intent intent = getIntent();
         String imageURI = intent.getStringExtra(DisplayData.IMAGE_URI);
 
+        //Back to Display Activity arrow
+        ImageView backArrow = (ImageView) findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //Show exit message
+        Toast.makeText(DisplayFullImage.this, "Deslize hacia abajo para salir", Toast.LENGTH_LONG).show();
+
+
+
+
+        //Universal Image Loader
         ImageLoader imageLoader = ImageLoader.getInstance();
 
         DisplayImageOptions options = new DisplayImageOptions.Builder().cacheInMemory(true)
@@ -53,6 +76,31 @@ public class DisplayFullImage extends ActionBarActivity {
                 imageProgressBar.setVisibility(View.GONE);
             }
         });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getY();
+                float deltaX = x2 - x1;
+                if (Math.abs(deltaX) > MIN_DISTANCE)
+                {
+                    //Toast.makeText(this, "left2right swipe", Toast.LENGTH_SHORT).show ();
+                    finish();
+                }
+                else
+                {
+                    // consider as something else - a screen tap for example
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
 
--- app/src/main/java/com/example/jose/utplqr/MainActivity.java
+++ app/src/main/java/com/example/jose/utplqr/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 
